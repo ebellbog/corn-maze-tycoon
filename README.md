7d64a368608549ecdcc7082e5316cf2bc2e8c568
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2025-11-15 20:21:29 -0500

    Initial project template

---
+++ README.md
@@ -0,0 +1,2 @@
+# Corn Maze Tycoon
+Prototype for an indie game about haunted corn mazes
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.less';
+import './mobile.less';
+
+let $body;
+
+$(document).ready(() => {
+    $body = $('body');
+    console.log('Ready to go!');
+});
